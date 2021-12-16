423035817906a5a0d90a9644726a75938dd09ffd
Elizabeth <elizabeth.abdysalam@gmail.com>
2021-12-16 21:13:50 +0600

    hooks

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TodoContainer from './containers/TodoList/TodoContainer';
 import TodoItemComponent from './containers/TodoList/TodoItemComponent';
 import ZamirPostsContainer from './containers/ZamirPosts/ZamirPosts'
-import UsersContainer from './containers/Users/UsersContainer';
+// import UsersContainer from './containers/Users/UsersContainer';
 import AijPostsContainer from './containers/AijPosts/AijPosts';
 import GulzhanPostsContainer from './containers/GulzhanPosts/GulzhanPostsContainer';
 import MansurPostsContainer from './containers/MansurPosts/MansurPosts';
 import AisuluuUsers from './containers/AisuluuUsers/AisuluuUsers'
+import EllePostsComp from './containers/AijPosts/hooks/EllePostsComp'
 import App from './App';
 
 import './index.css';
@@ -22,7 +23,7 @@ ReactDOM.render(
           <Route path=":itemId" element={<TodoItemComponent />} />
         </Route>
         
-        <Route path="users" element={<UsersContainer/>} />
+        <Route path="users" element={<EllePostsComp/>}/>
         <Route path="aijposts" element={<AijPostsContainer/>} />
         <Route path="mansur-posts" element={<MansurPostsContainer/>} />
         <Route path="zamir-posts" element={<ZamirPostsContainer/>} />
