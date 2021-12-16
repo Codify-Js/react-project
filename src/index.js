e90f42c41d3be3847f9a229e9c9e4fcf93d7fb91
Gulzhan <isoikigai@gmail.com>
2021-12-16 21:05:16 +0600

    hooks

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import AijPostsContainer from './containers/AijPosts/AijPosts';
 import GulzhanPostsContainer from './containers/GulzhanPosts/GulzhanPostsContainer';
 import MansurPostsContainer from './containers/MansurPosts/MansurPosts';
 import AisuluuUsers from './containers/AisuluuUsers/AisuluuUsers'
+import GulzhanPostsComp from './containers/GulzhanPosts/hooks/GulzhanPostsComp'
 import App from './App';
 
 import './index.css';
@@ -22,7 +23,7 @@ ReactDOM.render(
           <Route path=":itemId" element={<TodoItemComponent />} />
         </Route>
         
-        <Route path="users" element={<UsersContainer/>} />
+        <Route path="users" element={<GulzhanPostsComp/>} />
         <Route path="aijposts" element={<AijPostsContainer/>} />
         <Route path="mansur-posts" element={<MansurPostsContainer/>} />
         <Route path="zamir-posts" element={<ZamirPostsContainer/>} />
