fdbc265b403187eef4f118bf8023a3fa6b65bfa1
aisuluuisakova <isakovaaisuluu.com>
2021-12-16 21:05:19 +0600

    aisuluu-hooks

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import AijPostsContainer from './containers/AijPosts/AijPosts';
 import GulzhanPostsContainer from './containers/GulzhanPosts/GulzhanPostsContainer';
 import MansurPostsContainer from './containers/MansurPosts/MansurPosts';
 import AisuluuUsers from './containers/AisuluuUsers/AisuluuUsers'
+import AisuluuPostsComp from './containers/AisuluuUsers/hooks/AisuluuPostsContainer'
 import App from './App';
 
 import './index.css';
@@ -27,7 +28,7 @@ ReactDOM.render(
         <Route path="mansur-posts" element={<MansurPostsContainer/>} />
         <Route path="zamir-posts" element={<ZamirPostsContainer/>} />
         <Route path="gulzhan-posts" element={<GulzhanPostsContainer/>} />
-        <Route path="AisuluuUsers" element={<AisuluuUsers/>} />
+        <Route path="AisuluuUsers" element={<AisuluuPostsComp/>} />
 
         <Route path="*" element={<div>NOT FOUND</div>  } />
       </Route>
