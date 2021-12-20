e13be7d495182243a63466fae81414a7e14e364b
Gulzhan <isoikigai@gmail.com>
2021-12-20 23:23:04 +0600

    added GulzhanPostListContainer.jsx homework

--- src/containers/GulzhanPosts/GulzhanPostListContainer.jsx
+++ src/containers/GulzhanPosts/GulzhanPostListContainer.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { JSON_PLACEHOLDER_API_URL } from '../../components/constants/gulzhan-api'
-import { Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col, Button, Badge} from 'react-bootstrap';
 import InputComponent from '../../components/Input/InputComponent';
+import './GulzhanPostListContainer.css'
 
 const GulzhanPostListContainer = () => {
   const [posts, setPosts] = useState([])
   const [search, setSearch] = useState('')
-  const [clickedPost, setClickedPost] = useState('')
+  const [clickedPost, setClickedPost] = useState({})
   const [clickedPostId, setClickedPostId] = useState(0)
+  const [counter, setCounter] = useState(0)
 
   useEffect(() => {
     axios.get(`${JSON_PLACEHOLDER_API_URL}/posts`)
@@ -21,16 +23,15 @@ const GulzhanPostListContainer = () => {
     axios.get(`${JSON_PLACEHOLDER_API_URL}/posts/${clickedPostId}`)
     .then((response) => {
       setClickedPost (response.data)
-    })
-    
-  })
+    })},[counter])
 
   const handleInput = (event) => {
       setSearch(event.target.value)
   }
 
-  const handlePostClick = (event) => {
-    setClickedPostId(event.target.id)
+  const handlePostClick = (id) => {
+    setClickedPostId(id)
+    setCounter(counter+1)
   }
 
   const filteredPosts = posts.filter(post => post.title.toLowerCase().includes(search.toLowerCase()) || post.body.toLowerCase().includes(search.toLowerCase()))
@@ -38,26 +39,31 @@ const GulzhanPostListContainer = () => {
 
   return (
     <>
-      <h1 style={{color: 'gray', textAlign: 'center'}}>All Posts</h1>
+      <h1>All Posts</h1>
 
-      <Container>
-      <div className='searchInputArea' style={{display: 'flex', justifyContent:'center', padding: '20px'}}>
+      <Container className='postList-container'  style={{padding: '50px'}}>
+      <div className='searchInputArea'>
         <InputComponent placeholder='Search' value={search} onChange={handleInput}/>
       </div>      
-      <Row className="post">
+      <Row className='clickedPostDisplay'>
         POST: {clickedPost ? clickedPost.title : 'No clicked post'}
       </Row>
-      <Row style={{background: 'gray'}}>
-        <Col sm={1}>ID</Col>
-        <Col sm={4}>Post</Col>
-        <Col sm={7}>Description</Col>
+      <div className='displayClickCounter'>
+        <Button variant="light">
+          Counter <Badge bg="secondary">{counter}</Badge>
+        </Button>
+      </div>     
+      <Row className='postList-columnTitles'>
+        <Col className='column' sm={1}>ID</Col>
+        <Col className='column' sm={2}>Post</Col>
+        <Col className='column' sm={9}>Description</Col>
       </Row>
 
       {filteredPosts.map(post => (
-        <Row onClick={handlePostClick}>
-          <Col sm={1}>{post.id}</Col>  
-          <Col sm={4}>{post.title}</Col>
-          <Col sm={7}>{post.body}</Col>
+        <Row key={post.id} className='postList-posts' onClick={() => handlePostClick(post.id)}>
+          <Col className='column' sm={1}>{post.id}</Col>  
+          <Col className='column' sm={2}>{post.title}</Col>
+          <Col className='column' sm={9}>{post.body}</Col>
         </Row>
       ))}
       </Container>
@@ -66,4 +72,4 @@ const GulzhanPostListContainer = () => {
     )
   }
 
-export default GulzhanPostListContainer
+  export default GulzhanPostListContainer
