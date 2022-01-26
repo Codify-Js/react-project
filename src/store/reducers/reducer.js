d3c42bbb435d00f509be4ed7cce43be5c11e08d5
Mansur Mabo <mm@codemotion.eu>
2022-01-26 12:00:21 +0600

    add post crud

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -22,7 +22,29 @@ export default function reducer(state = initState, action) {
     case actions.POST_CREATE_RESPONSE:
       return {
         ...state,
-        createdPosts: [...state.createdPosts, action.payload],
+        createdPosts: [...state.createdPosts, {...action.payload, id: state.createdPosts.length + 1}],
+      }
+    case actions.POST_DELETE:
+      // const filteredCreatedPosts = state.createdPosts.filter(item => item.id !== action.postId)
+      const filteredCreatedPosts = [...state.createdPosts]
+      const target = filteredCreatedPosts.find(item => item.id === action.postId)
+      const targetIndex = filteredCreatedPosts.indexOf(target)
+      filteredCreatedPosts.splice(targetIndex, 1)
+      
+      return {
+        ...state,
+        createdPosts: filteredCreatedPosts,
+      }
+
+    case actions.POST_UPDATE:
+      const newPosts = [...state.createdPosts]
+      const targetPost = newPosts.find(item => item.id === action.payload.id);
+      const index = newPosts.indexOf(targetPost);
+      newPosts[index] = action.payload;
+
+      return {
+        ...state,
+        createdPosts: newPosts,
       }
     default:
       return state;
